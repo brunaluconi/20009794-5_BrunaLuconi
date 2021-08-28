3101b1b6c0e6d9a74ef8659e557413356c47d0da
Bruna Luconi <89212675+brunaluconi@users.noreply.github.com>
2021-08-27 22:35:53 -0300

    pasta com arquivo de dados

---
+++ dados/dados.php
@@ -0,0 +1,20 @@
+<?php
+
+return $historia = array('description' => 'O Ateliê dos Doces tem como proposta, produzir e fornecer doces variados com alto padrão de qualidade sem perder a característica de doces caseiros. A empresa familiar surgiu em 2000 na cidade de Blumenau, em Santa Catarina. Sua fundadora, Betina Wissmann, iniciou a produção de doces por hobby, para agradar os filhos, o que acabou virando uma paixão.
+Com muito esforço e sacrifício, em 2007, abriu seu próprio cantinho chamado "Ateliê dos Doces".');
+
+return $doces = array(
+	0 => array('id' => 1, 'title' => 'Brigadeiros', 'imagem' => 'images/brigadeiros.jpg', 'description' => 'Exprimente nosso delicioso brigadeiro. Sabores: chocolate belga, meio amargo, ninho ou churros.'),
+	1 => array('id' => 2, 'title' => 'Cookies', 'imagem' => 'images/cookies.jpg', 'description' => 'Experimente nossos cookies americanos. Sabores: chocolate com gotas de chocolate branco, massa tradicional com gotas de chocolate ou massa tradicional com passas.'),
+	2 => array('id' => 3, 'title' => 'Cupcakes', 'imagem' => 'images/cupcakes.jpg', 'description' => 'Nosso mini bolinho de massa de chocolate ou manteiga, Sabores: chocolate com ou sem recheio, manteiga com ou sem recheio.'),
+	3 => array('id' => 4, 'title' => 'Brownies', 'imagem' => 'images/brownies.jpg', 'description' => 'Nossos brownies molhadinhos são uma delícia. Sabores: brownies com pedacinhod de chocolate, mini marshmallows ou nozes.'),
+);
+
+return $dados = array(
+	0 => array('title' => 'Brigadeiros', 'description' => 'Feitos com leite condensado com ou sem lactose, manteiga, chocolate Belga, chocolate meio amargo leite ninho ou churros e cobertos com sprits de chocolate.'),
+	1 => array('title' => 'Cookies', 'description' => 'Massa que leva manteiga, açucar branco, açucar mascavo, farinha, fermento, ovos e essência de baunilha ou chocolate em pó. Podem ser de chocolate com gotas de chocolate branco, tradicionais com gotas de chocolate ou com passas.'),
+	2 => array('title' => 'Cupcakes', 'description' => 'Massa fofinha de chocolate ou não, leva ovos, essência de baunilha, leite, óleo, farinha e fermento. Podem ser de chocolate, tradicionais, com ou sem recheio'),
+	3 => array('title' => 'Brownies', 'description' => 'Levar achocolatado em pó ou chocolate, ao leite ou meio amargo, farinha, açucar, ovos, fermento e manteiga, Pode ter pedacinhos de chocolate, mini marshmallows ou nozes.'),
+);
+
+
